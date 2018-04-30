71f84cf3f867fa1784cb76c7d5ed9f5fefb6be48
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-04-30 23:33:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+//Test 1 -- Passed
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+//Test 2 -- Passed 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+//Test 3 -- Passed
+function deepestChild() {
+  var store = document.getElementById("grand-node").querySelectorAll("div");
+  var deepest;
+  for(let i=0; i<store.length - 1; i++) {
+    deepest = store[i].querySelector("div");
+  }return deepest;
+}
