6475cbefc644d5bd500cddc93998d3f7d218ec26
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-05-01 00:02:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getFirstSelector(selector) {
 
 //Test 2 -- Passed 
 function nestedTarget() {
-  return document.getElementById('nested').querySelector('.target');
+  return document.getElementById("nested").querySelector(".target");
 }
 
 //Test 3 -- Passed
@@ -17,10 +17,10 @@ function deepestChild() {
   }return deepest;
 }
 
-//Test 4 --
+//Test 4 -- Passed
 function increaseRankBy(n) {
   let list = document.getElementById("app").querySelectorAll("ul.ranked-list li");
-  for(var i=0;i<list.length;i++) {
+  for(var i=0; i<list.length; i++) {
     list[i].innerHTML = (parseInt(list[i].innerHTML)) + n;
   }
 }
